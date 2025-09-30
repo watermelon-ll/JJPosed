5125ce75df3fbbe1717f46811b78a509bf36075e
赛骏 <saijun.ljj@autonavi.com>
2025-09-30 17:50:39 +0800

    build修复

--- daemon/build.gradle.kts
+++ daemon/build.gradle.kts
@@ -115,6 +115,7 @@ android.applicationVariants.all {
 
 dependencies {
     implementation(libs.libxposed.api)
+    implementation("io.github.libxposed:interface:100")
     implementation("io.github.libxposed:service:100")
     implementation(libs.agp.apksig)
     implementation(libs.commons.lang3)
